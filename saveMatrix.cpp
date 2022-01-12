d6e349afbdc9f4b76ab9cb6e61fa8d21ce81de23
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-01-12 19:26:44 +0000

    Add .cpp file for saveMatrix.h

---
+++ saveMatrix.cpp
@@ -0,0 +1,17 @@
+#include "saveMatrix.h"
+
+void saveMatrix::saveData(std::string filePath, std::string fileName, Eigen::MatrixXd matrix)
+{
+    filePath_in = filePath;
+    fileName_in = fileName;
+    matrix_in = matrix;
+
+    const static Eigen::IOFormat CSVFormat(Eigen::StreamPrecision, Eigen::DontAlignCols, ", ", "\n");
+
+    std::ofstream file(filePath_in+"\\"+fileName_in);
+    if (file.is_open())
+    {
+        file << matrix_in.format(CSVFormat);
+        file.close();
+    }
+}
