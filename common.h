451a18e760ea5e230d471f16ffea1fdab69bb798
Cameron McEleney <c.mceleney.1@research.gla.ac.uk>
2022-01-12 17:41:19 +0000

    Place to save typedefs

---
+++ common.h
@@ -0,0 +1,10 @@
+//
+// Created by Cameron McEleney on 12/01/2022.
+//
+
+#ifndef SPINCHAINS_COMMON_H
+#define SPINCHAINS_COMMON_H
+
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic> MatrixcXd;
+
+#endif //SPINCHAINS_COMMON_H
